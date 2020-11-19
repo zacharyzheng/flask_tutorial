fda6e0f472031c6c5235207d0ca2f1a7c9571237
zachary <zachary.zheng@hotmail.com>
2020-11-19 05:45:46 -0800

    add migrations

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = 'zachary@a9182HE'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://debian-sys-maint:AmZam5dO7i5uC0fW@localhost:3306/flaskblog?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
